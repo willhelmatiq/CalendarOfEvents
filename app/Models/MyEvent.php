edb46bf953c37ce1375872df41ad95c93cf5e7f3
willhelmatiq <rot3ad@gmail.com>
2023-01-17 19:35:20 +0100

    medialibrary update

--- app/Models/MyEvent.php
+++ app/Models/MyEvent.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class MyEvent extends Model
+class MyEvent extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
@@ -19,5 +24,17 @@ class MyEvent extends Model
     public function participants()
     {
         return $this->belongsToMany(User::class, 'user_myevent', 'user_id', 'event_id', 'id', 'id');
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('thumbnail')
+            ->fit(Manipulations::FIT_CROP, 50, 50)
+            ->nonQueued();
+        $this
+            ->addMediaConversion('header')
+            ->fit(Manipulations::FIT_CROP, 800, 200)
+            ->nonQueued();
     }
 }
