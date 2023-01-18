c5a8493035c4683ef3d5de8be922b0b81faf0451
willhelmatiq <rot3ad@gmail.com>
2023-01-18 23:43:51 +0100

    add backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use function PHPUnit\Framework\at;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +17,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:run --only-db')->dailyAt('23:50')->timezone('Europe/Brussels');
+        $schedule->command('backup:run')->dailyAt('01:00')->timezone('Europe/Brussels');
+
     }
 
     /**
