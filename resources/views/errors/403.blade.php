fb4a352be87fc22c1341bab9e5f88e9dd26c1017
willhelmatiq <rot3ad@gmail.com>
2023-01-26 07:08:40 +0100

    add errors

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+<x-site-layout>
+
+{{--    <div >--}}
+{{--        <img src="{{'http://calendarofevents.test:8080/media/2/404dayheader-1.png'}}" class="w-full object-cover">--}}
+{{--    </div>--}}
+    <div class="bg-red-500 text-red-50 text-6xl font-bold rounded w-full h-screen">
+        This is my custom 404
+    </div>
+
+</x-site-layout>
