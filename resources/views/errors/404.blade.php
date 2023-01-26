d69b441cb73b566c9170b3497601240970bf4aec
willhelmatiq <rot3ad@gmail.com>
2023-01-26 09:04:29 +0100

    add subscribes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,7 @@
 {{--        <img src="{{'http://calendarofevents.test:8080/media/2/404dayheader-1.png'}}" class="w-full object-cover">--}}
 {{--    </div>--}}
     <div class="bg-red-500 text-red-50 text-6xl font-bold rounded w-full h-screen">
-        This is my custom 403
+        This is my custom 404
     </div>
 
 </x-site-layout>
